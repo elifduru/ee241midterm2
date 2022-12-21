9df04e838854bdcc126dd150339a71b77167ae0c
elifduru <120724591+elifduru@users.noreply.github.com>
2022-12-21 19:56:27 +0300

    Add files via upload

---
+++ elif_duru_q1/question_1_midterm_2.srcs/sim_1/new/main_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.12.2022 18:25:56
+// Design Name: 
+// Module Name: main_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main_tb;
+reg a ;
+wire b ;
+main dut(.a(a),.b(b));
+
+endmodule
---
+++ elif_duru_q2/quesiton_2_midterm_2.srcs/sim_1/new/main_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.12.2022 18:39:45
+// Design Name: 
+// Module Name: main_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main_tb;
+reg a;
+wire b;
+main dut(.a(a),.b(b));
+endmodule
